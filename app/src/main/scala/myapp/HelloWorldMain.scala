05e8175a19543e55b48ee06b82905548dec29178
Patrick Sy <patrick.sy@get-it.us>
2020-04-21 03:20:22 +0200

    step 5

--- app/src/main/scala/myapp/HelloWorldMain.scala
+++ app/src/main/scala/myapp/HelloWorldMain.scala
@@ -3,14 +3,24 @@ package myapp
 import org.scalajs.dom
 import org.scalajs.dom.document
 
-import scala.scalajs.js.annotation.JSExportTopLevel
-
 object HelloWorldMain {
+
   def main(args: Array[String]): Unit = {
-    println("Hello World")
-    appendPar(document.body, "Hello World1")
+    document.addEventListener("DOMContentLoaded", { (e: dom.Event) =>
+      setupUI()
+    })
   }
 
+  def setupUI(): Unit = {
+    val button = document.createElement("button")
+    button.textContent = "Click me!"
+    button.addEventListener("click", { (e: dom.MouseEvent) =>
+      addClickedMessage()
+    })
+    document.body.appendChild(button)
+
+    appendPar(document.body, "Hello World")
+  }
 
   def appendPar(targetNode: dom.Node, text: String): Unit = {
     val parNode = document.createElement("p")
@@ -18,7 +28,6 @@ object HelloWorldMain {
     targetNode.appendChild(parNode)
   }
 
-  @JSExportTopLevel("addClickedMessage")
   def addClickedMessage(): Unit = {
     appendPar(document.body, "You clicked the button!")
   }
